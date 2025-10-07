16a78b616b019c0a75684c870d6dd60603d3232b
Gabriel Alpendre <gabrielalpendre@users.noreply.github.com>
2025-10-07 00:36:38 -0300

    hotfix: readme and swarm deploy script

--- docker-swarm-deploy.sh
+++ docker-swarm-deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-docker info > /dev/null 2>&1
-if [ $? -ne 0 ]; then
-  echo "Docker Swarm não está inicializado. Inicializando..."
-  docker swarm init
+SWARM_STATUS=$(docker info --format '{{.Swarm.LocalNodeState}}')
+echo "Current Swarm status: $SWARM_STATUS"
+if [ "$SWARM_STATUS" != "active" ]; then
+    docker swarm init
 fi
 
 echo "Building API image..."
